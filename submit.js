4886583767a0b1a46589fca1ca9ae3c1a138c2f1
Athos-Of-Oa <114652935+Athos-Of-Oa@users.noreply.github.com>
2023-05-15 13:33:31 +0100

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,1 +1,42 @@
-
+let form = document.getElementById("profanityForm");
+form.addEventListener("submit", function () 
+{
+  let textArea = document.getElementById("inspectPost");
+  let uName = document.getElementById("inspectUser");
+  const post = textArea.value.split(" "); // Inspected Post array
+  let length = post.length;
+  const users = [
+    ['AthosOfOa', 0] , 
+    ['c0ncreet', 2], 
+    ['bees', 3]]; //List of users and how many strikes on their account
+  const banned = ["apple", "orange", "banana", "apricot", "peach", "grape", "apples", "oranges", "bananas", "apricots", "peaches", "grapes"]; // Simulated Profanity
+  let allowed = true;
+  for (var i = 0; i< users.length; i++)
+  {
+    if (uName = users[0][i])
+    {
+      
+    }
+  }
+  for (var i = 0; i < length; i++) 
+  {
+    for (var j = 0; j < banned.length; j++) 
+    {
+      if (post[i] == banned[j]) 
+      {
+        allowed = false;
+        post[i] = banned[j];
+        break;
+      }
+    }
+  }
+  if (allowed == false) 
+  {
+    alert('A Banned word has been found, the user will have one(1) strike added to their account.');
+    
+  }
+  else 
+  {
+    alert('no banned words were detected');
+  }
+});
